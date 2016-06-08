697bcf857bb3c2c53552b4be1cc73de71d783a46
social-track.uk <phil@social-track.uk>
2016-06-08 13:11:18 +0100

    Form handling

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
 
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser());
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
     res.send('got you');
