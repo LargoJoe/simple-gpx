b03d9c52a0ca46f53bc651f15b5fe8587540aa94
social-track.uk <phil@social-track.uk>
2016-06-08 14:14:23 +0100

    Form handling

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+app.use(bodyParser());
 
 app.post('/', function (req, res) {
     var parseString = xml2js.parseString;
