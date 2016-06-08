a38896a5114f748a54758fb286f24cc0cc4b6306
social-track.uk <phil@social-track.uk>
2016-06-08 16:13:26 +0100

    Form handling

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/', upload.single('fileinput'), function (req, res) {
     var gpx = req.file.buffer.toString();
     var parseString = xml2js.parseString;
     parseString(gpx, function (err, result) {
-        var trk = result.gpx.trk[0].trkseg;
+        var trk = result.gpx.trk[0].trkseg[0].trkpt[0];
 
         res.send(trk);
     });
