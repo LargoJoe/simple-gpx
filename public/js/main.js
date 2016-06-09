09d8c15d04856dbc5dc8d1a2bd9b35737aef48ae
social-track.uk <phil@social-track.uk>
2016-06-09 14:46:04 +0100

    Form handling

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,11 @@
 $(document).ready(function () {
     $('#fileinput').on('change',
             function (e) {
-
                 $('#gpx-simplify-form').submit();
             });
     $('#tolerance').on('change',
             function (e) {
-                if $('#fileinput').val().length > 0) {
+                if ($('#fileinput').val().length > 0) {
                     $('#gpx-simplify-form').submit();
                 }
             });
