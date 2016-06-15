5306a6bd71d4590da2ad207e87aa8e9a0c1fa4ef
social-track.uk <phil@social-track.uk>
2016-06-15 13:39:34 +0100

    Simplify trksegs

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.post('/', upload.single('fileinput'), function (req, res) {
             return;
         }
 
-        console.log(JSON.stringify(result.gpx));
 
 
-        result.gpx.$.creator = "Simple GPX https://simple-gpx.herokuapp.com ";
+
+        result.gpx.$.creator = "Simple GPX https://simple-gpx.herokuapp.com";
         var tracks = result.gpx.trk;
         var t = tracks.length;
 
