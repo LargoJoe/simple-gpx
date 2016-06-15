9a3793705e1b0b7cd17c880029c4b44ec4249752
social-track.uk <phil@social-track.uk>
2016-06-15 14:04:07 +0100

    Simplify trksegs

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/', upload.single('fileinput'), function (req, res) {
 
                     var split_name = req.body.splitname === "" ? "Track" : req.body.splitname;
                     var last_split = 0;
-                    if (typeof splits !== 'undefined') {
+                    if (typeof splits === 'undefined') {
                         var splits = 1;
                     }
 
