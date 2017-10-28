b76f640697b995b4742260a6c2b21d7ab2363587
social-track.uk <phil@social-track.uk>
2017-10-28 18:38:00 +0100

    Procfile update

--- index.js
+++ index.js
@@ -91,6 +91,11 @@ app.post('/', upload.single('fileinput'), function (req, res) {
                 delete result.gpx.trk[i].trkseg[t];
             }
 
+            if (typeof result.gpx.trk[i].trkseg != "undefined") {
+                delete result.gpx.trk[i].trkseg;
+            }
+
+
 
             /*
              * Simplify and replace trkpoints with simplified trkpoints for
@@ -119,12 +124,13 @@ app.post('/', upload.single('fileinput'), function (req, res) {
 
             }
 
+
+
             var hmac = crypto.createHmac('sha256', 'This is not a phrase you will guess');
             var track_string = JSON.stringify(formatted_pts);
             hmac.update(track_string);
-            var cmt = 'HMAC Digest: ' + hmac.digest('base64');
+            var cmt = 'HMAC Digest: ' + hmac.digest('hex');
             result.gpx.trk[i].cmt = cmt;
-            console.log(JSON.stringify(result.gpx.trk[i]));
 
             result.gpx.trk[i].trkseg[0] = {};
             result.gpx.trk[i].trkseg[0].trkpt = formatted_pts;
