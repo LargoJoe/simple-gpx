0286ac0c30619bc2e7741fe5c0a83ebe42b9549f
social-track.uk <phil@social-track.uk>
2017-10-28 17:28:37 +0100

    Procfile update

--- index.js
+++ index.js
@@ -115,14 +115,14 @@ app.post('/', upload.single('fileinput'), function (req, res) {
 
             }
 
+            result.gpx.trk[i].trkseg[0] = {}
+            result.gpx.trk[i].trkseg[0].trkpt = formatted_pts;
+
             var hmac = crypto.createHmac('sha256', 'This is not a phrase you will guess');
             var track_string = JSON.stringify(formatted_pts);
             hmac.update(track_string);
             var cmt = "HMAC Digest: " + hmac.digest('hex');
             result.gpx.trk[i].cmt = cmt;
-
-            result.gpx.trk[i].trkseg[0] = {}
-            result.gpx.trk[i].trkseg[0].trkpt = formatted_pts;
 
 
             /*
