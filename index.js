180ee314ae6fa5a0538c2d04b98da77912428232
social-track.uk <phil@social-track.uk>
2018-07-25 12:23:14 +0100

    polyline

--- index.js
+++ index.js
@@ -262,7 +262,6 @@ app.post('/', upload.single('fileinput'), function (req, res) {
                     rte.name = split_name + '-' + r; // route_name
                     r++;
                     rte.rtept = formatted_rtepts;
-                    console.log(rte);
                     result.gpx.rte.push(rte);
                     formatted_rtepts = [];
                 }
