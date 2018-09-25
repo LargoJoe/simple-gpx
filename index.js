e9bd5b73e5d4bb443cc74b933aee5410462ecead
social-track.uk <phil@social-track.uk>
2018-09-25 11:52:18 +0100

    Waypoint proximity alarms

--- index.js
+++ index.js
@@ -34,21 +34,25 @@ app.post('/', upload.single('fileinput'), function (req, res) {
 
 
         result.gpx.$.creator = "Simple GPX https://simple-gpx.herokuapp.com";
-        // Garmin Waypoint extensions
-        result.gpx.$["xmlns:wptx1"] = "http://www.garmin.com/xmlschemas/WaypointExtension/v1";
-        // Garmin Waypoint proximity alarms
-        if (typeof result.gpx.wpt !== "undefined") {
 
-            for (i = 0; i < result.gpx.wpt.length; i++) {
 
-                if (result.gpx.wpt[i].extensions === undefined) {
-                    result.gpx.wpt[i].extensions = {};
+        if (req.body.proximityalarm !== "none") {
+            // Garmin Waypoint extensions
+            result.gpx.$["xmlns:wptx1"] = "http://www.garmin.com/xmlschemas/WaypointExtension/v1";
+            // Garmin Waypoint proximity alarms
+            if (typeof result.gpx.wpt !== "undefined") {
+
+                for (i = 0; i < result.gpx.wpt.length; i++) {
+
+                    if (result.gpx.wpt[i].extensions === undefined) {
+                        result.gpx.wpt[i].extensions = {};
+                    }
+                    console.log(result.gpx.wpt[i].extensions);
+                    if (result.gpx.wpt[i].extensions["wptx1:WaypointExtension"] === undefined) {
+                        result.gpx.wpt[i].extensions["wptx1:WaypointExtension"] = {};
+                    }
+                    result.gpx.wpt[i].extensions["wptx1:WaypointExtension"]["wptx1:Proximity"] = req.body.proximityalarm;
                 }
-                console.log(result.gpx.wpt[i].extensions);
-                if (result.gpx.wpt[i].extensions["wptx1:WaypointExtension"] === undefined) {
-                    result.gpx.wpt[i].extensions["wptx1:WaypointExtension"] = {};
-                }
-                result.gpx.wpt[i].extensions["wptx1:WaypointExtension"]["wptx1:Proximity"] = 150;
             }
         }
 
